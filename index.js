31c9cdf22661e402848c3e78fb543d1d7098d865
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-10 01:29:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ function total() {
 }
 
 function removeFromCart(item) {
-  let index = 0; index < cart.length; index++
-  if (cart[index].itemName === item) {
+  if (for (let index = 0; index < cart.length; index++)
+   cart[index].itemName === item) {
     //for  (let index = 0; index < cart.length; index++)
     cart.splice(index, 1)
     return cart
